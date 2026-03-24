6788e40857bce6d53f6bded2206a315bc0075d55
leandro matias silva <leandroms165@hotmail.com>
2026-03-24 07:51:57 -0300

    Geração de relatorios

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,15 @@ const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
 
 module.exports = defineConfig({
+
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: true
+  },
+  
   e2e: {
     specPattern: "cypress/e2e/**/*.feature",
 
